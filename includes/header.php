a26164561c66ec71891a16aa5c9ee70c1c03a701
Fardin Rahman <mfardinr@gmail.com>
2024-09-11 00:56:25 +0600

    Refactor header links to use relative paths

--- includes/header.php
+++ includes/header.php
@@ -30,7 +30,7 @@
             <button class="navbar-toggler navbar-toggler-right" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
             <div class="collapse navbar-collapse" id="navbarResponsive">
                 <ul class="navbar-nav ms-auto my-2 my-lg-0">
-                    <li class="nav-item"><a class="nav-link" href="/index.php/#page-top">Home</a></li>
+                    <li class="nav-item"><a class="nav-link" href="../index.php/#page-top">Home</a></li>
                     <li class="nav-item"><a class="nav-link" href="../index.php/#movie">Movie</a></li>
                     <li class="nav-item"><a class="nav-link" href="../index.php/#portfolio">TODO</a></li>
                     <li class="nav-item"><a class="nav-link" href="../index.php/#contact">Contact</a></li>
