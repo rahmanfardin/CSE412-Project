3e354355a2d9f3df2e97b04cab3c71f229f1340d
Fardin Rahman <mfardinr@gmail.com>
2024-09-22 01:02:54 +0600

    v2.2

--- index.php
+++ index.php
@@ -35,6 +35,7 @@ if (isset($_SESSION['username'])) {
     <!-- Navigation-->
     <nav class="navbar navbar-expand-lg navbar-light fixed-top py-3" id="mainNav">
         <div class="container px-4 px-lg-5">
+        <img src="./assets/favicon.ico" alt="">
             <a class="navbar-brand" href="#page-top">TICKETER</a>
             <button class="navbar-toggler navbar-toggler-right" type="button" data-bs-toggle="collapse"
                 data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false"
