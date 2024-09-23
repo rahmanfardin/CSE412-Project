f2b59e4f50d5cfb9b78b4b925bdfd8ebfa05b73f
Fardin Rahman <mfardinr@gmail.com>
2024-09-23 19:12:46 +0600

    minor bug fix in navigation - Refactor header logo and link in admin and public pages

--- includes/header.php
+++ includes/header.php
@@ -27,8 +27,8 @@
     <nav class="navbar navbar-expand-lg navbar-light fixed-top py-3" id="mainNav">
         <div class="container px-4 px-lg-5">
             
-        <img src="./assets/favicon.ico" alt="">
-            <a class="navbar-brand" href="#page-top"> TICKETER</a>
+        <!-- <img src="./assets/favicon.ico" alt=""> -->
+            <a class="navbar-brand" href="../index.php#page-top"><img src="./assets/favicon.ico" alt=""> TICKETER</a>
             <button class="navbar-toggler navbar-toggler-right" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
             <div class="collapse navbar-collapse" id="navbarResponsive">
                 <ul class="navbar-nav ms-auto my-2 my-lg-0">
@@ -37,7 +37,7 @@
                     <li class="nav-item"><a class="nav-link" href="../ticket.php">Ticket</a></li>
                     <li class="nav-item"><a class="nav-link" href="../index.php#contact">Contact</a></li>
                    
-                    <li class="nav-item"><a class="nav-link" href="#login">LOGIN/SIGNUP</a></li>
+                    <li class="nav-item"><a class="nav-link" href="./login.php">LOGIN/SIGNUP</a></li>
                 </ul>
             </div>
         </div>
--- index.php
+++ index.php
@@ -35,8 +35,8 @@ if (isset($_SESSION['username'])) {
     <!-- Navigation-->
     <nav class="navbar navbar-expand-lg navbar-light fixed-top py-3" id="mainNav">
         <div class="container px-4 px-lg-5">
-        <img src="./assets/favicon.ico" alt="">
-            <a class="navbar-brand" href="#page-top"> TICKETER</a>
+        <!-- <img src="./assets/favicon.ico" alt=""> -->
+            <a class="navbar-brand" href="#page-top"><img src="./assets/favicon.ico" alt=""> TICKETER</a>
             <button class="navbar-toggler navbar-toggler-right" type="button" data-bs-toggle="collapse"
                 data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false"
                 aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
