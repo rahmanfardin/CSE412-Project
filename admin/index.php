2de23c31bc3f5f40f90022d202bc9310dcfa9107
Fardin Rahman <mfardinr@gmail.com>
2024-09-23 18:47:16 +0600

    Refactor header logo and link in admin and public pages

--- admin/index.php
+++ admin/index.php
@@ -11,7 +11,7 @@ include './includes/header.php';
     <div class="container px-4 px-lg-5 h-100">
         <div class="row gx-4 gx-lg-5 h-100 align-items-center justify-content-center text-center">
             <div class="col-lg-8 align-self-end">
-                <h1 class="text-white font-weight-bold">Your Favorite Place for Movie ticket solution</h1>
+                <h1 class="text-white font-weight-bold">Admin panel for Movie ticket solution</h1>
                 <hr class="divider" />
             </div>
             <div class="col-lg-8 align-self-baseline">
--- index.php
+++ index.php
@@ -36,7 +36,7 @@ if (isset($_SESSION['username'])) {
     <nav class="navbar navbar-expand-lg navbar-light fixed-top py-3" id="mainNav">
         <div class="container px-4 px-lg-5">
         <img src="./assets/favicon.ico" alt="">
-            <a class="navbar-brand" href="#page-top">TICKETER</a>
+            <a class="navbar-brand" href="#page-top"> TICKETER</a>
             <button class="navbar-toggler navbar-toggler-right" type="button" data-bs-toggle="collapse"
                 data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false"
                 aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
